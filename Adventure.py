a915714266b0489b13f86213df2ff86967a54661
Michael Suter <suterm@hartwick.edu>
2019-11-02 20:45:28 -0400

    added basic def statements. will work more sunday and monday

--- Adventure.py
+++ Adventure.py
@@ -1,1 +1,24 @@
+#Alex Bello
+#Aly Batch
+#Mike Suter
+
+import random
+
+
+prizes = ["a_new_car", "a_trip_to_the_bahamas", "a_banana", "$25,000", "a_years_supply_of_gummy_bears"]
+
+trivia_questions = ["", "", "", "4", "5"]
+
+
+def play_game():
+    pick = random.choice(trivia_questions)
+    print(pick)
+
+
+def pick_prize():
+    print(random.choice(prizes))
+
+
+play_game()
+pick_prize()
 
