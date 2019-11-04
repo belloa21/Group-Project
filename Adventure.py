4fcc39373274c2421529171f8e6cc201d091bee7
Michael Suter <suterm@hartwick.edu>
2019-11-04 11:07:31 -0500

    framework

--- Adventure.py
+++ Adventure.py
@@ -7,20 +7,43 @@ import random
 
 prizes = ["a_new_car", "a_trip_to_the_bahamas", "a_banana", "$25,000", "a_years_supply_of_gummy_bears"]
 
-trivia_questions = ["Do bears roar?", "Are all cats evil?", "Are cars faster than planes?", "Can chickens fly?", "Is Lichtman a good teacher?"]
+trivia_questions = ["Do bears roar?n", "Are all cats evil?n", "Are cars faster than planes?n", "Can chickens fly?n", "Is Lichtman a good teacher?y"]
 
 
 def play_game():
-    for question in trivia_questions:
-        answer = input("Answer Y for yes or N for no. >>")
-        if answer == 'Y' or 'y':
-            print("Correct!! Here is your prize!")
-            pick_prize()
-        else:
-            if answer == 'N' or 'n':
-                print("Wrong answer, you suck at this!")
-    pick = random.choice(trivia_questions)
-    print(pick)
+    list_pick = random.randint(1, 5)
+    if list_pick == 1:
+        question = trivia_questions[0]
+        position = question.find('?')
+        print(question[0:position+1])
+        print(question[position+1:position:2])
+    elif list_pick == 2:
+        question = trivia_questions[1]
+        position = question.find('?')
+        print(question[0:position+1])
+        print(question[position+1:position:2])
+    elif list_pick == 3:
+        question = trivia_questions[2]
+        position = question.find('?')
+        print(question[0:position+1])
+        print(question[position+1:position:2])
+    elif list_pick == 4:
+        question = trivia_questions[3]
+        position = question.find('?')
+        print(question[0:position+1])
+        print(question[position+1:position:2])
+    elif list_pick == 5:
+        question = trivia_questions[4]
+        position = question.find('?')
+        print(question[0:position+1])
+        print(question[position+1:position:2])
+    answer = input("Answer Y for yes or N for no. >>")
+    if answer == 'Y' or 'y':
+        print("Correct!! Here is your prize!")
+        pick_prize()
+    else:
+        if answer == 'N' or 'n':
+            print("Wrong answer, you suck at this!")
 
 
 def pick_prize():
@@ -28,5 +51,3 @@ def pick_prize():
 
 
 play_game()
-pick_prize()
-
